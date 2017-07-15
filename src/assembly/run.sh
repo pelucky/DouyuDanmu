42e87d7fb45d2fe44d04eaac72677374978ca7e0
pelucky <zx8522588@gmail.com>
2017-07-15 19:43:55 +0800

    change config

--- src/assembly/run.sh
+++ src/assembly/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p logs
-JVM_OPTS='-XX:+TieredCompilation -Xmx1024m -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:logs/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=1 -XX:GCLogFileSize=100M'
+JVM_OPTS='-XX:+TieredCompilation -Xmx1024m -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:logs/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=1 -XX:GCLogFileSize=100M -Dfile.encoding=UTF-8'
 
 CLASS_PATH="danmu-0.0.1-SNAPSHOT.jar":./conf
 
